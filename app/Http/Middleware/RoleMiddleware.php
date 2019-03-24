53b1de4b6547a6e5298c2c49a7640fe1a1d60c4e
Lutfi Ghazali <zillucks988@gmail.com>
2019-03-24 20:00:44 +0700

    Update Role based Authentication

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -15,7 +15,7 @@ class RoleMiddleware
      */
     public function handle($request, Closure $next, ...$roles)
     {
-        if (!$request->user()->hasRole($roles)) {
+        if (!$request->user() || !$request->user()->hasRole($roles)) {
             // abort(401, 'Unauthorized User.');
             \Alert::error('Unauthorized User', 'Error 401');
             return redirect()->intended('/');
